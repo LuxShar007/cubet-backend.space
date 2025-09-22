08be7903c182640ddfc19e7b56eb4ba92776565e
Sharvin Mhatre <86613145+LuxShar007@users.noreply.github.com>
2025-09-22 17:39:09 +0530

    Update spotify.js

--- api/login/spotify.js
+++ api/login/spotify.js
@@ -1,7 +1,7 @@
 // This function handles the initial login redirect to Spotify.
 export default function handler(req, res) {
     const scope = 'user-read-private user-read-email';
-    const SPOTIFY_REDIRECT_URI = `https://your-vercel-project-name.vercel.app/api/callback/spotify`;
+    const SPOTIFY_REDIRECT_URI = `https://cubet-backend-space.vercel.app/api/callback/spotify`;
 
     const authUrl = 'https://accounts.spotify.com/authorize?' + new URLSearchParams({
         response_type: 'code',
@@ -11,4 +11,5 @@ export default function handler(req, res) {
     }).toString();
 
     res.redirect(307, authUrl);
+
 }
